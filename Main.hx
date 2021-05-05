20d7e247df8eccfa4a5da5dd80065113b296d566
PXshadow <joeyprather8@gmail.com>
2021-05-04 18:14:21 -0700

    check ls before make

--- Main.hx
+++ Main.hx
@@ -15,6 +15,7 @@ private function setupLinux() {
     Sys.command("sudo apt-get update -qqy & sudo apt-get install -qqy cmake make libpng-dev libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev"); //dependencies
     Sys.command("git clone https://github.com/HaxeFoundation/hashlink");
     Sys.setCwd("hashlink"); //change dir to hashlink
+    Sys.command("ls");
     Sys.command("sudo make all");
     Sys.command("sudo make install");
     Sys.command("ls");
