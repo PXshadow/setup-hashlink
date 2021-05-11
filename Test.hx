16d76ecd4a18cfee48a72bd9168d452e69754321
PXshadow <joeyprather8@gmail.com>
2021-05-10 23:21:55 -0700

    attempt to test all hashlink included libs

--- Test.hx
+++ Test.hx
@@ -1,3 +1,11 @@
+import hl.uv.Tcp;        //uv
+import sys.db.Mysql;     //mysql
+import sys.db.Sqlite;    //sqlite
+import sys.ssl.Socket;   //ssl
+import hl.Format;        //fmt
+import hl.UI.Window;     //ui
+import sdl.GL;           //sdl
+import openal.AL;        //al
 function main() {
     trace("Hello world");
 }
