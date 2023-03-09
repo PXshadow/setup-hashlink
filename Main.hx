eac15738d90573118d0dabe4617e4de85bfebc4b
Joey <9813078+PXshadow@users.noreply.github.com>
2023-03-09 09:47:49 +0100

    update versions of Haxe, and action deps

--- Main.hx
+++ Main.hx
@@ -15,7 +15,7 @@ function main() {
         case "Linux":
             deleteDirectoryRecursively("hashlink");
             Sys.println("---------------------");
-	    Sys.command("sudo apt-get update");
+	        Sys.command("sudo apt-get update");
             Sys.command("sudo apt-get install libpng-dev libturbojpeg-dev libvorbis-dev libopenal-dev libsdl2-dev libmbedtls-dev libuv1-dev");
             Sys.command("sudo git clone https://github.com/HaxeFoundation/hashlink");
             Sys.setCwd("hashlink");
